23b3248de3ba8b60ee4e3558255c642c6999a101
Dinh Cuong Hoang <cghg@tsv-learning-desktop>
2021-07-07 17:35:52 +0200

    add graspnet basline

--- examples/exam_vis.py
+++ examples/exam_vis.py
@@ -5,7 +5,7 @@ __version__ = '1.0'
 # change the graspnet_root path
 
 ####################################################################
-graspnet_root = '/home/gmh/graspnet' # ROOT PATH FOR GRASPNET
+graspnet_root = '/graspnet' # ROOT PATH FOR GRASPNET
 ####################################################################
 
 from graspnetAPI import GraspNet
@@ -14,13 +14,13 @@ from graspnetAPI import GraspNet
 g = GraspNet(graspnet_root, camera='kinect', split='train')
 
 # show object grasps
-g.showObjGrasp(objIds = 0, show=True)
+#g.showObjGrasp(objIds = 0, show=True)
 
 # show 6d poses
-g.show6DPose(sceneIds = 0, show = True)
+#g.show6DPose(sceneIds = 0, show = True)
 
 # show scene rectangle grasps
-g.showSceneGrasp(sceneId = 0, camera = 'realsense', annId = 0, format = 'rect', numGrasp = 20)
+#g.showSceneGrasp(sceneId = 0, camera = 'realsense', annId = 0, format = 'rect', numGrasp = 20)
 
 # show scene 6d grasps(You may need to wait several minutes)
 g.showSceneGrasp(sceneId = 4, camera = 'kinect', annId = 2, format = '6d')
